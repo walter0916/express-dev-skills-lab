92d67990fce3d932bad63827f90b572cee4130a2
WalterGuerra <wsteven00@gmail.com>
2023-09-14 13:38:15 -0400

    finish part 1

--- routes/skills.js
+++ routes/skills.js
@@ -1,10 +1,9 @@
 import { Router } from 'express'
+import * as skillsCtrl from '../controller/skills.js'
 
 const router = Router()
 
 // GET localhost:3000/users
-router.get('/', function(req, res) {
-  res.send('respond with a resource')
-})
+router.get('/', skillsCtrl.index)
 
 export { router }
--- server.js
+++ server.js
@@ -4,10 +4,11 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import createError from 'http-errors'
 import logger from 'morgan'
+import methodOverride from 'method-override'
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
-import { router as usersRouter } from './routes/users.js'
+import { router as skillsRouter } from './routes/skills.js'
 
 // create the express app
 const app = express()
@@ -24,10 +25,11 @@ app.use(
     path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')
   )
 )
+app.use(methodOverride('_method'))
 
 // mount imported routes
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/skills', skillsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
